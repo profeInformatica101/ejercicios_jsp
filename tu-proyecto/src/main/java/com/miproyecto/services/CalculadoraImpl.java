d42709d462dd1640333e5455a4cd4e2f588afca3
Juan <jdommay863@g.educaand.es>
2024-11-08 07:58:18 +0100

    añadido estwructura base

---
+++ tu-proyecto/src/main/java/com/miproyecto/services/CalculadoraImpl.java
@@ -0,0 +1,29 @@
+package com.miproyecto.services;
+
+public class CalculadoraImpl implements Calculadora{
+
+	
+	@Override
+	public double sumar(double a, double b) {
+		return  a + b;
+	}
+
+	@Override
+	public double restar(double a, double b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double multiplicar(double a, double b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double dividir(double a, double b) throws ArithmeticException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ tu-proyecto/src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inicio</title>
+</head>
+<body>
+    <h1>Hola desde index.jsp!</h1>
+    <a href="miServlet">Ir a MiServlet</a>
+    <a href="suma.jsp?numero1=10&numero2=20">Suma.jsp</a>
+</body>
+</html>
